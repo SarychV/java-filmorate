72265bde36cab82675d030bb7612a5542d1f4817
SarychV <sarychevv@mail.ru>
2023-08-08 23:58:27 +0700

    fix: TZ10 After review#1

--- src/main/java/ru/yandex/practicum/filmorate/model/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/NotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
-public class NotFoundException extends RuntimeException{
+public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }
 
